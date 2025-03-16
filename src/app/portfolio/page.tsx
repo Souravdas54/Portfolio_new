7e7ae42fc9e8a5ea5b1a039b2cc07b722274946d
Sourav Das <sd.sourav54@gmail.com>
2025-03-16 10:34:22 +0530

    Update page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -41,7 +41,7 @@ const Home: React.FC = () => {
 
         {/* Open Graph (OG) - For social sharing */}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Frontend Developer Portfolio" />
+        <meta property="og:title" content="Portfolio - Frontend Developer Portfolio" />
         <meta property="og:description" content="Explore my portfolio showcasing React.js, Next.js, and JavaScript projects." />
         <meta property="og:image" content="/image/1741623665154.jpg" />
         <meta property="og:url" content="https://portfolio-new-five-eosin.vercel.app/" />
@@ -49,7 +49,7 @@ const Home: React.FC = () => {
 
         {/* Twitter Cards */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Frontend Developer Portfolio" />
+        <meta name="twitter:title" content="Portfolio - Frontend Developer Portfolio" />
         <meta name="twitter:description" content="Explore my portfolio showcasing React.js, Next.js, and JavaScript projects." />
         <meta name="twitter:image" content="/image/1741623665154.jpg" />
 
